4651018184924ee30fb36cdb3295e5341bf4e241
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-17 09:31:30 +0545

    Learnt Typecasting

---
+++ week1/type-casting.py
@@ -0,0 +1,19 @@
+'''
+Typecasting -> changing one datatype to another datatype
+
+Two typecasting methods :
+
+- Implicit 
+    => Performed automatically by python's compiler to prevent data loss
+    => example : 145.5 converted to 146
+- Explicit 
+    => Explicitly converting datatype using typecasting function such as str(), int(),
+    float(), ord(), hex(), tuple(), list(), dict()
+'''
+
+
+x= str('11')
+print(type(x))
+y= float('10')
+print(type(y))
+print(y)
