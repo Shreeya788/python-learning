d3ddf7100e215b2dae317ec2701d44684238926a
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-20 22:36:31 +0545

    Nested Loops with their time complexity

--- week1/Looping/Nested-Loops/simpleNested.py
+++ week1/Looping/Nested-Loops/simpleNested.py
@@ -1,4 +1,8 @@
-for i in range(10):
-    for j in range(10):
+import time
+start_time = time.time()
+
+for i in range(100):
+    for j in range(100):
         print(0, end=' ')
     print()
+print(round((time.time() - start_time),2))  #Time complexity
