458a6cdb41d8ffcaf382aa47320e33f3732057b0
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-20 18:16:08 +0545

    Learnt while loop in python

---
+++ week1/Looping/whileLoop/Example1.py
@@ -0,0 +1,7 @@
+favorites = ['Creme Brulee', 'Apple pie', 'Churros','Tiramisu','Chocolate Cake']
+
+count = 0
+
+while count < len(favorites):
+    print('I like this desser', favorites[count])
+    count +=1
