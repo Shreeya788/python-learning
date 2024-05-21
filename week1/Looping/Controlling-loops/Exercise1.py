e7e36f5ca373435b849374edf25a0cb9d1288287
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-21 19:23:09 +0545

    Controlling loops Example

--- week1/Looping/Controlling-loops/Exercise1.py
+++ week1/Looping/Controlling-loops/Exercise1.py
@@ -1,3 +1,14 @@
 num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
-for num in num_list:
-    print(num)
+count = 0
+for x, num in enumerate(num_list):
+    if num > 45:
+        print('Over 45')
+    else:
+        print('Under 45')
+    
+for x, num in enumerate(num_list):
+    count += 1
+    if num == 36:
+        print('Number found at ',x)
+        break
+print(count)
