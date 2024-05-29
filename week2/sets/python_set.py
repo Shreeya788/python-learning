08dee55276d9fe7345edd39148856f63c08d4667
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-29 22:46:30 +0545

    Added set introduction

---
+++ week2/sets/python_set.py
@@ -0,0 +1,12 @@
+'''
+Set is a collection which is unordered, unchangeable and unindexed.
+
+Sets are written with curly brackets and used to store multiple items in a single variable.
+
+set is unchangeable but we can add new items or remove existing items from it.
+it doesn't allow duplicate values
+
+values True and 1 are considered same in set Likewise False and O are considered same
+
+set() constructor can be used to make a set
+'''
